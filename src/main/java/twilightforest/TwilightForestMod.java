b539b44b3cd8006a624ae5ea1f34da2f182e879f
Johann Bernhardt <johann.bernhardt@tum.de>
2021-12-09 13:37:47 +0100

    Migrate to unified build script

--- src/main/java/twilightforest/TwilightForestMod.java
+++ src/main/java/twilightforest/TwilightForestMod.java
@@ -63,7 +63,7 @@ import twilightforest.world.WorldProviderTwilightForest;
 public class TwilightForestMod {
 
     public static final String ID = "TwilightForest";
-    public static final String VERSION = "2.3.8dev";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
 
     public static final String MODEL_DIR = "twilightforest:textures/model/";
     public static final String GUI_DIR = "twilightforest:textures/gui/";
